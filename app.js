f74ad15f0487a4959896812b5ba022ec4100246c
andy-dev <andres.macedo@gmail.com>
2015-06-18 19:42:05 -0700

    add handlebars example

--- app.js
+++ app.js
@@ -1,17 +1,50 @@
 
-var divElements = document.getElementsByClassName('divStyle')
+// var divElements = document.getElementsByClassName('divStyle')
 
-for(var i=0; i<= divElements.length; i++){
-  divElements[i].onclick = function(event){
-    event = event || window.event; //ie8 get the event object
+// console.log(divElements)
+// for(var i=0; i<= divElements.length; i++){
+//   divElements[i].onclick = function(event){
+//     event = event || window.event; //ie8 get the event object
 
-    if(event.stopPropagation){ // if this is supported use this ie9 other browsers
-      event.stopPropagation();
-    }
-    else {
-      event.cancelBubble = true // else use this one
-    }
-    this.style.borderColor = "red";
-    alert(this.getAttribute("id") + " border color changed")
+//     if(event.stopPropagation){ // if this is supported use this ie9 other browsers
+//       event.stopPropagation();
+//     }
+//     else {
+//       event.cancelBubble = true // else use this one
+//     }
+//     this.style.borderColor = "red";
+//     alert(this.getAttribute("id") + " border color changed")
+//   }
+// }
+
+
+// // map function
+// var myArray = [4,3,5,2,1]
+
+// var newArray= myArray.map(function(num){
+//   return num * 2;
+// })
+
+// console.log(myArray)
+// console.log(newArray)
+
+// handlebar example
+
+$(document).ready(function(){
+
+  //one we get our template
+  var source= $("#new_template").html();
+  //two we create the template
+  var template = Handlebars.compile(source);
+
+  var context = {
+    title: "this is our title from far away",
+    body: "this is our sexy body"
   }
-}
+
+  var el_html = template(context);
+
+  $("#render_one").html(el_html);
+
+
+})
