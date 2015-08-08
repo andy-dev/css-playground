7256fd4adc405805b0f7f91167320097343111d4
andy-dev <andres.macedo@gmail.com>
2015-08-07 19:45:34 -0700

    playing with canvas

--- app.js
+++ app.js
@@ -98,17 +98,7 @@ var myModule = function () {
 // two functions have to be return that have a closure over private scope
 // access the internal state and that is called a module
 
-//---------------------------
-//  Simple unit test
-//---------------------------
-// var Bob = require('./bob.js');
-// describe("Bob", function() {
-//   var bob = new Bob();
-//   it("stating something", function() {
-//     var result = bob.hey('Tom-ay-to, tom-aaaah-to.');
-//     expect(result).toEqual('Whatever.');
-//   });
-// });
+
 
 //---------------------------
 //  Nested Function
@@ -141,3 +131,26 @@ var player = {
 player.addWeapon("samurai sword")
 
 console.log(player.weapons)
+
+//---------------------------
+//  Canvas
+//---------------------------
+
+var canvas = document.getElementById("canvasOne");
+var ctx = canvas.getContext("2d");
+
+ctx.fillStyle = "rgb(200, 0, 200)";
+ctx.fillRect(50, 40, 100, 30);
+
+
+//---------------------------
+//  Simple unit test
+//---------------------------
+// var Bob = require('./bob.js');
+// describe("Bob", function() {
+//   var bob = new Bob();
+//   it("stating something", function() {
+//     var result = bob.hey('Tom-ay-to, tom-aaaah-to.');
+//     expect(result).toEqual('Whatever.');
+//   });
+// });
